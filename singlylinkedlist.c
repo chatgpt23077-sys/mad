c69665aa0a5cabfd955467d73119cf53e18051a3
chatgpt23077-sys <chatgpt23077@gmail.com>
2025-12-12 20:20:18 +0530

    Create singlylinkedlist.c

---
+++ singlylinkedlist.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+struct Node *head = NULL;
+
+void insertEnd(int value) {
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+    struct Node *temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+}
+
+void deleteValue(int value) {
+    if (head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+
+    struct Node *temp = head, *prev = NULL;
+
+    if (temp->data == value) {
+        head = temp->next;
+        free(temp);
+        return;
+    }
+
+    while (temp != NULL && temp->data != value) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL)
+        printf("Value not found\n");
+    else {
+        prev->next = temp->next;
+        free(temp);
+    }
+}
+
+void display() {
+    struct Node *temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    insertEnd(10);
+    insertEnd(20);
+    insertEnd(30);
+    printf("List: ");
+    display();
+
+    deleteValue(20);
+    printf("After deleting 20: ");
+    display();
+
+    return 0;
+}
