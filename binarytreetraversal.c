a496c5f85f16bc371d737a42a41440ebc976e53e
chatgpt23077-sys <chatgpt23077@gmail.com>
2025-12-12 20:26:23 +0530

    Create binarytreetraversal.c

---
+++ binarytreetraversal.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* insert() {
+    int value;
+    printf("Enter value (-1 for no node): ");
+    scanf("%d", &value);
+
+    if (value == -1)
+        return NULL;
+
+    struct Node* newNode = createNode(value);
+    printf("Enter left child of %d\n", value);
+    newNode->left = insert();
+    printf("Enter right child of %d\n", value);
+    newNode->right = insert();
+
+    return newNode;
+}
+
+void inorder(struct Node* root) {
+    if (root == NULL) return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+void preorder(struct Node* root) {
+    if (root == NULL) return;
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(struct Node* root) {
+    if (root == NULL) return;
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
+}
+
+int main() {
+    struct Node* root = NULL;
+    printf("Create the binary tree:\n");
+    root = insert();
+
+    printf("\nInorder Traversal: ");
+    inorder(root);
+
+    printf("\nPreorder Traversal: ");
+    preorder(root);
+
+    printf("\nPostorder Traversal: ");
+    postorder(root);
+
+    return 0;
+}
